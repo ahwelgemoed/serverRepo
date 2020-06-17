063e595363c53da73b9edbda72720f6e010f6072
ahwelgemoed <arno.welgemoed@outlook.com>
2020-06-17 20:26:08 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -33,21 +33,3 @@ const insertData = async ({ temperature, humidity }: IncomingData) => {
   const db = await openDb();
   await db.exec(`INSERT INTO weather_data VALUES (${temperature},${humidity})`);
 };
-
-// // const DBSOURCE = 'db.sqlite';
-// const c = async () => {
-//   const db = await openDb();
-//   console.log('b', db);
-//   try {
-//     await db.exec('CREATE TABLE tbl (col TEXT)');
-//     await db.exec('INSERT INTO tbl VALUES ("test")');
-//   } catch (error) {
-//     await db.exec('INSERT INTO tbl VALUES ("test")');
-//     // console.log('error', error);
-//   }
-
-//   const nn = await db.get('SELECT col FROM tbl WHERE col = ?', 'test');
-//   console.log('nn', nn);
-// };
-
-// c();
