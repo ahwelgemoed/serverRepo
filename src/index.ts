69c59f6f4224ee45f42a3726f7aad71a8b357cc2
ahwelgemoed <arno.welgemoed@outlook.com>
2020-06-18 07:57:42 +0200

    dateTime

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,9 @@ wss.on('connection', (ws) => {
 const saveDataToDB = async (data: IncomingData) => {
   const db = await openDb();
   try {
-    await db.exec('CREATE TABLE weather_data (temp, humi)');
+    await db.exec(
+      'CREATE TABLE weather_data (id INTEGER PRIMARY KEY, temperature VARCHAR(25) NOT NULL, humidity VARCHAR(25) NOT NULL, date DATE)'
+    );
     await insertData(data);
   } catch (error) {
     await insertData(data);
@@ -30,6 +32,14 @@ const saveDataToDB = async (data: IncomingData) => {
 };
 
 const insertData = async ({ temperature, humidity }: IncomingData) => {
-  const db = await openDb();
-  await db.exec(`INSERT INTO weather_data VALUES (${temperature},${humidity})`);
+  try {
+    const db = await openDb();
+    const date = new Date();
+    const sqliteDate = date.toISOString();
+    await db.exec(
+      `INSERT INTO weather_data VALUES (${null}, ${temperature},${humidity},DATETIME('now'))`
+    );
+  } catch (error) {
+    console.log('error', error);
+  }
 };
